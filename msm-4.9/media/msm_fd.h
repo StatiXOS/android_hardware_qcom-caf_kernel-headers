8c94114eaf726fee16c8f45b2d21cb7ccda49518
Sourajit Karmakar <sourajitk@statixos.com>
2023-01-29 15:48:42 +0000

    msm-4.9: update kernel headers
    from: LA.UM.10.3.r1-01700-sdm845.0
    
    Change-Id: I784755ad45cf11f2b79ef90287608bd57ad95670

--- msm-4.9/media/msm_fd.h
+++ msm-4.9/media/msm_fd.h
@@ -1,57 +1,76 @@
-/****************************************************************************
- ****************************************************************************
- ***
- ***   This header was automatically generated from a Linux kernel header
- ***   of the same name, to make information necessary for userspace to
- ***   call into the kernel available to libc.  It contains only constants,
- ***   structures, and macros generated from the original header, and thus,
- ***   contains no copyrightable information.
- ***
- ***   To edit the content of this header, modify the corresponding
- ***   source file (e.g. under external/kernel-headers/original/) then
- ***   run bionic/libc/kernel/tools/update_all.py
- ***
- ***   Any manual change here will be lost the next time this script will
- ***   be run. You've been warned!
- ***
- ****************************************************************************
- ****************************************************************************/
 #ifndef __UAPI_MSM_FD__
 #define __UAPI_MSM_FD__
+
 #include <linux/videodev2.h>
 #include <linux/types.h>
+
+/*
+ * struct msm_fd_event - Structure contain event info.
+ * @buf_index: Buffer index.
+ * @frame_id: Frame id.
+ * @face_cnt: Detected faces.
+ */
 struct msm_fd_event {
-  __u32 buf_index;
-  __u32 frame_id;
-  __u32 face_cnt;
+	__u32 buf_index;
+	__u32 frame_id;
+	__u32 face_cnt;
 };
+
+/*
+ * enum msm_fd_pose - Face pose.
+ */
 enum msm_fd_pose {
-  MSM_FD_POSE_FRONT,
-  MSM_FD_POSE_RIGHT_DIAGONAL,
-  MSM_FD_POSE_RIGHT,
-  MSM_FD_POSE_LEFT_DIAGONAL,
-  MSM_FD_POSE_LEFT,
+	MSM_FD_POSE_FRONT,
+	MSM_FD_POSE_RIGHT_DIAGONAL,
+	MSM_FD_POSE_RIGHT,
+	MSM_FD_POSE_LEFT_DIAGONAL,
+	MSM_FD_POSE_LEFT,
 };
+
+/*
+ * struct msm_fd_face_data - Structure contain detected face data.
+ * @pose: refer to enum msm_fd_pose.
+ * @angle: Face angle
+ * @confidence: Face confidence level.
+ * @reserved: Reserved data for future use.
+ * @face: Face rectangle.
+ */
 struct msm_fd_face_data {
-  __u32 pose;
-  __u32 angle;
-  __u32 confidence;
-  __u32 reserved;
-  struct v4l2_rect face;
+	__u32 pose;
+	__u32 angle;
+	__u32 confidence;
+	__u32 reserved;
+	struct v4l2_rect face;
 };
+
+/*
+ * struct msm_fd_result - Structure contain detected faces result.
+ * @frame_id: Frame id of requested result.
+ * @face_cnt: Number of result faces, driver can modify this value (to smaller)
+ * @face_data: Pointer to array of face data structures.
+ *  Array size should not be smaller then face_cnt.
+ */
 struct msm_fd_result {
-  __u32 frame_id;
-  __u32 face_cnt;
-  struct msm_fd_face_data * face_data;
+	__u32 frame_id;
+	__u32 face_cnt;
+	struct msm_fd_face_data __user *face_data;
 };
-#define VIDIOC_MSM_FD_GET_RESULT _IOWR('V', BASE_VIDIOC_PRIVATE, struct msm_fd_result)
+
+/* MSM FD private ioctl ID */
+#define VIDIOC_MSM_FD_GET_RESULT \
+	_IOWR('V', BASE_VIDIOC_PRIVATE, struct msm_fd_result)
+
+/* MSM FD event ID */
 #define MSM_EVENT_FD (V4L2_EVENT_PRIVATE_START)
-#define V4L2_CID_FD_SPEED (V4L2_CID_PRIVATE_BASE)
-#define V4L2_CID_FD_FACE_ANGLE (V4L2_CID_PRIVATE_BASE + 1)
-#define V4L2_CID_FD_MIN_FACE_SIZE (V4L2_CID_PRIVATE_BASE + 2)
-#define V4L2_CID_FD_FACE_DIRECTION (V4L2_CID_PRIVATE_BASE + 3)
-#define V4L2_CID_FD_DETECTION_THRESHOLD (V4L2_CID_PRIVATE_BASE + 4)
-#define V4L2_CID_FD_WORK_MEMORY_SIZE (V4L2_CID_PRIVATE_BASE + 5)
-#define V4L2_CID_FD_WORK_MEMORY_FD (V4L2_CID_PRIVATE_BASE + 6)
+
+/* MSM FD control ID's */
+#define V4L2_CID_FD_SPEED                (V4L2_CID_PRIVATE_BASE)
+#define V4L2_CID_FD_FACE_ANGLE           (V4L2_CID_PRIVATE_BASE + 1)
+#define V4L2_CID_FD_MIN_FACE_SIZE        (V4L2_CID_PRIVATE_BASE + 2)
+#define V4L2_CID_FD_FACE_DIRECTION       (V4L2_CID_PRIVATE_BASE + 3)
+#define V4L2_CID_FD_DETECTION_THRESHOLD  (V4L2_CID_PRIVATE_BASE + 4)
+#define V4L2_CID_FD_WORK_MEMORY_SIZE     (V4L2_CID_PRIVATE_BASE + 5)
+#define V4L2_CID_FD_WORK_MEMORY_FD       (V4L2_CID_PRIVATE_BASE + 6)
+
 #endif
 

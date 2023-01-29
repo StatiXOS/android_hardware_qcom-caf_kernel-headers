66eeefc62d7239a4a0a5c5225a402ef642f731d8
Sourajit Karmakar <sourajitk@statixos.com>
2023-01-29 15:48:42 +0000

    kernel-headers: update msm-4.9 kernel headers
    from: LA.UM.10.3.r1-01700-sdm845.0
    
    Change-Id: I784755ad45cf11f2b79ef90287608bd57ad95670

--- msm-4.9/media/msm_jpeg_dma.h
+++ msm-4.9/media/msm_jpeg_dma.h
@@ -1,29 +1,28 @@
-/****************************************************************************
- ****************************************************************************
- ***
- ***   This header was automatically generated from a Linux kernel header
- ***   of the same name, to make information necessary for userspace to
- ***   call into the kernel available to libc.  It contains only constants,
- ***   structures, and macros generated from the original header, and thus,
- ***   contains no copyrightable information.
- ***
- ***   To edit the content of this header, modify the corresponding
- ***   source file (e.g. under external/kernel-headers/original/) then
- ***   run bionic/libc/kernel/tools/update_all.py
- ***
- ***   Any manual change here will be lost the next time this script will
- ***   be run. You've been warned!
- ***
- ****************************************************************************
- ****************************************************************************/
+/* Copyright (c) 2015-2016, 2018, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #ifndef __UAPI_MSM_JPEG_DMA__
 #define __UAPI_MSM_JPEG_DMA__
+
 #include <linux/videodev2.h>
+
+/* msm jpeg dma control ID's */
 #define V4L2_CID_JPEG_DMA_SPEED (V4L2_CID_PRIVATE_BASE)
 #define V4L2_CID_JPEG_DMA_MAX_DOWN_SCALE (V4L2_CID_PRIVATE_BASE + 1)
-struct msm_jpeg_dma_buff {
-  int32_t fd;
-  uint32_t offset;
-};
-#endif
 
+/* msm_jpeg_dma_buf */
+struct msm_jpeg_dma_buff {
+	int32_t fd;
+	uint32_t offset;
+};
+
+#endif /* __UAPI_MSM_JPEG_DMA__ */
